88645940496d8cf9fc87036cf0242f4409a79d72
mtdemircan <celticheroee@gmail.com>
2021-06-03 11:09:04 +0300

    new

--- views/functions.php
+++ views/functions.php
@@ -3,10 +3,17 @@
         return view('index');
     }
 
-    function tab1(){
-        $configPath = extensionDb('newHostName');
-        return respond($configPath,200);
+    function tab1(){     
+         
+        $b = extensionDb('newHostName');
+        $a=runCommand("hostname");
+        $c=runCommand("hostname --fqdn");
+        $message=$a." ".$b." ".$c;
+        
+        return respond($message,200);
     }
+
+    
 
     function tab2(){
         return respond(runCommand("cat /etc/hosts"),200);
@@ -20,6 +27,16 @@
         runCommand(sudo()."hostnamectl set-hostname test");
         runCommand(sudo()."hostname test");
         runCommand(sudo()."sed -i 's/$currentHostname/test/g' /etc/hosts");
+        return respond("ok",200);
+        
+    }
+    function changeHost(){
+        $newHostname = request("new");
+
+        $currentHostname=runCommand("hostname");
+        runCommand(sudo()."hostnamectl set-hostname $newHostname");
+        runCommand(sudo()."hostname $newHostname");
+        runCommand(sudo()."sed -i 's/$currentHostname/$newHostname/g' /etc/hosts");
         return respond("ok",200);
         
     }
--- views/index.blade.php
+++ views/index.blade.php
@@ -14,16 +14,45 @@
 </ul>
 
 <div class="tab-content" id="nav-tabContent">
-    <div class="tab-pane active" id="tab1" role="tabpanel" value="test" ></div>
+    <div class="tab-pane active" id="tab1" role="tabpanel" value="test" >
+        <div class="table-responsive" id="hostname"></div>
+    </div>
     <pre class="tab-pane fade" id="tab2" role="tabpanel"></pre>
     <div class="tab-pane fade" id="tab3" role="tabpanel">
         <button class="btn btn-success mb-2" id="tab3button" onclick="hostNameButtonClicked()" type="button">hostname</button>
         <div class="table-responsive" id="hostname"></div>
+        <button type="button" class="btn btn-info btn-lg" data-toggle="modal" data-target="#myModal">Open Modal</button>
     </div>
     <div class="tab-pane fade" id="tab4" role="tabpanel">
         <button class="btn btn-success mb-2" id="tab4button" onclick="neww()" type="button">hostname change</button>
         <div class="table-responsive" id="hostname"></div>
+        <!-- <form action="/functions.php" method="post"> -->
+            <label for="fname">New Hostname:</label>
+            <input type="text" id="fname" name="fname"><br><br>
+            <input type="submit" value="Submit" onclick="newHostnameEntered()">
+        <!-- </form> -->
     </div>
+</div>
+
+<div id="myModal" class="modal fade" role="dialog">
+  <div class="modal-dialog">
+
+    <!-- Modal content-->
+    <form action="functions.php" method="post">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h4 class="modal-title">Metin Girisi</h4>
+                <button type="button" class="close" data-dismiss="modal">&times;</button>
+            </div>
+            <div class="modal-body">
+                <input type="text" id="modaltext" name="modaltext" placeholder="metin giriniz">
+            </div>
+            <div class="modal-footer">
+                <button type="submit" class="btn btn-default" data-dismiss="modal" onclick="dialogClosed()">OK</button>
+            </div>
+        </div>
+    </form>
+  </div>
 </div>
 
 <!-- <div class="tab-content">
@@ -50,15 +79,32 @@
         tab1();
     }
 
+    function dialogClosed() {
+        var dialogMessage = document.getElementById('modaltext').value;
+        showSwal(dialogMessage, 'success', 5000);
+    }
+
     function tab1(){
-        console.log(document.getElementById('tab1').getAttribute('value'));
+        //console.log(document.getElementById('tab1').getAttribute('value'));
         var form = new FormData();
         request("{{API('tab1')}}", form, function(response) {
             message = JSON.parse(response)["message"];
-            $('#tab1').html(message);
+            showSwal(message, 'success', 3000);
+            var a=message.substring(0,message.indexOf(" "));
+            message=message.substring(message.indexOf(" ")+1);
+            var b=message.substring(0,message.indexOf(" "));
+            message=message.substring(message.indexOf(" ")+1);
+            var c=message;
+            $('#tab1').html("current hostname:"+a+"</br>settings hostname:"+b+"</br>current fqdn:"+c);
+            if(a==b)
+            showSwal("success", 'success', 5000);
+            else
+            showSwal("error", 'error', 5000);
+
         }, function(error) {
             $('#tab1').html("Hata oluştu");
         });
+        
     }
 
     function tab2(){        
@@ -98,6 +144,7 @@
         //     $('#tab3').html("Hata oluştu");
         // });
     }
+
     function neww(){   
         var form = new FormData();
         request("{{API('tab4')}}", form, function(response) {
@@ -105,6 +152,22 @@
             $('#tab4').html(message);
         }, function(error) {
             $('#tab4').html("Hata oluştu");
+        });
+    }
+    function newHostnameEntered() {
+
+        var newHost = document.getElementById('fname').value;
+        var form = new FormData();
+        form.id = "newHost";
+        form.name = newHost;
+        console.log(form);
+        console.log(request);
+        form.append("new", form.name);
+        request("{{API('changeHost')}}", form, function(response) {
+            message = JSON.parse(response)["message"];
+            showSwal(message, 'success', 5000);
+        }, function(error) {
+            $('#tab4').html(error);
         });
     }
 </script>
